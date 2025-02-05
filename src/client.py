30da6aa665a98f0ace8bf29c6fdc7d4f5941ac49
Chamber6821 <vania.osinin@yandex.ru>
2025-02-05 16:37:23 +0300

    Just do it

--- src/client.py
+++ src/client.py
@@ -1,15 +1,53 @@
+from common.Channel import Channel, Message
+from common.Router import Router
 from common.TcpConnection import ConnectionClosed
 from common.TcpSocket import TcpSocket
 import asyncio
 
+from common.messages import CloseMessage, EchoMessage, MessageType, TimeRequestMessage
+
+
+router = Router()
+
+
+@router.on(MessageType.ECHO)
+async def onEcho(message: Message):
+    print('Message:', message.body().decode())
+
+
+@router.on(MessageType.RETURN_TIME)
+async def onTime(message: Message):
+    print('Time:', float(message.body().decode()))
+
+
+@router.otherwise
+async def unknown(message: Message):
+    print('Unknown message:', message.type(), message.args(), message.body())
+
 
 async def main():
     connection = TcpSocket('127.0.0.1', 8080).connect() 
+    channel = Channel(connection)
     while True:
-        line = input()
-        await connection.sendLine(line)
-        line = await connection.receiveLine()
-        print(line, end='')
+        print('''
+        0 - EXIT
+        1 - TIME
+        2 - ECHO
+        ''')
+        choice = int(input())
+        match choice:
+            case 0:
+                await channel.send(CloseMessage())
+            case 1:
+                await channel.send(TimeRequestMessage())
+            case 2:
+                await channel.send(EchoMessage(text=input('Text: ')))
+            case _:
+                print('Unknown choice')
+                continue
+        response = await router.route(await channel.next())
+        if response:
+            raise Exception('Unexpected message after routing')
 
 
 try:
--- src/server.py
+++ src/server.py
@@ -1,13 +1,42 @@
+from common.Channel import Channel, Message
+from common.Router import Router
+from common.TcpConnection import ConnectionClosed
 from common.TcpSocket import TcpSocket
 from common.Connection import Connection
+import time
+
+from common.messages import EchoMessage, MessageType, TimeResponseMessage
+
+
+router = Router()
+
+
+@router.on(MessageType.CLOSE)
+async def onClose(_):
+    raise ConnectionClosed
+
+
+@router.on(MessageType.ECHO)
+async def onEcho(message: Message):
+    return message
+
+
+@router.on(MessageType.GET_TIME)
+async def onTime(_):
+    return TimeResponseMessage(timestamp=time.time())
+
+
+@router.otherwise
+async def unknown(message: Message):
+    return EchoMessage(text=f'Unknown command: {message.type().value} {message.args()} {message.body()}')
 
 
 async def handle(connection: Connection):
+    channel = Channel(connection)
     while True:
-        line = await connection.receiveLine()
-        print('Got', line.encode())
-        if line == 'exit\n': return
-        await connection.sendLine(line)
+        response = await router.route(await channel.next())
+        if response:
+            await channel.send(response)
 
 
 def main():

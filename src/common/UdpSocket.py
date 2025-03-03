dca9c47e23bb0399dd88157eb4f276e79dbb75b2
Hannah-Kaliada <120135661+Hannah-Kaliada@users.noreply.github.com>
2025-03-03 13:05:20 +0300

    Implement connection reuse in UdpSocket to route packets by address.

--- src/common/UdpSocket.py
+++ src/common/UdpSocket.py
@@ -1,10 +1,7 @@
 import socket
-import asyncio
 import traceback
-
 from common.UdpConnection import UdpConnection
 from common.Socket import Socket
-
 
 class UdpSocket(Socket):
     def __init__(self, ip, port) -> None:
@@ -14,15 +11,22 @@ class UdpSocket(Socket):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.ip, self.port))
+        self.connections = {}
 
     def connect(self, target_ip, target_port):
-        return UdpConnection(self.sock, (target_ip, target_port))
+        addr = (target_ip, target_port)
+        if addr not in self.connections:
+            self.connections[addr] = UdpConnection(self.sock, addr)
+        return self.connections[addr]
 
     def listen(self, handler):
         while True:
             try:
                 data, addr = self.sock.recvfrom(1024)
-                connection = UdpConnection(self.sock, addr)
+                if addr not in self.connections:
+                    self.connections[addr] = UdpConnection(self.sock, addr)
+
+                connection = self.connections[addr]
                 handler(connection, data, addr)
             except Exception:
                 print(traceback.format_exc())
